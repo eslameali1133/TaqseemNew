ab3a9013e500257a848246bf7728d63444fea1fe
Husseinomda16 <eslamali1133@hotmail.com>
2019-02-24 16:14:16 +0200

    Hussein Calendar

---
+++ Taqseem/Controller/BookPlayGroundVC.swift
@@ -0,0 +1,33 @@
+//
+//  BookPlayGroundVC.swift
+//  Taqseem
+//
+//  Created by Husseinomda16 on 2/24/19.
+//  Copyright © 2019 OnTime. All rights reserved.
+//
+
+import UIKit
+
+class BookPlayGroundVC: UIViewController {
+
+    @IBOutlet weak var txtDuration: UITextField!
+    @IBOutlet weak var txtTime: UITextField!
+    @IBOutlet weak var txtCity: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
