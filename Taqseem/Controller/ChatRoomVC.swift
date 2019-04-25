19943e94c132063cdf83d4dd46561f13dc7faca3
Husseinomda16 <eslamali1133@hotmail.com>
2019-04-25 12:08:59 +0200

    add pod

--- Taqseem/Controller/ChatRoomVC.swift
+++ Taqseem/Controller/ChatRoomVC.swift
@@ -6,56 +6,292 @@
 //  Copyright © 2019 OnTime. All rights reserved.
 //
 
-import UIKit
-
-class ChatRoomVC: UIViewController {
-
-     @IBOutlet weak var TBL_Chatroom: UITableView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        TBL_Chatroom.dataSource = self
-        TBL_Chatroom.delegate = self
-        TBL_Chatroom.changeView()
-        // Do any additional setup after loading the view.
+    import UIKit
+    import SwiftyJSON
+    class ChatRoomVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+        var http = HttpHelper()
+        var GroupID = "74"
+        var CurrentPlayer : User!
+        @IBOutlet weak var tableView: UITableView!
+        @IBOutlet weak var messageTextField: UITextField!
+        
+        @IBOutlet weak var inputContainerBottomContraint: NSLayoutConstraint!
+        
+        var activeUserLabel: UILabel!
+        var GroupMessages = [GroupMessageModelClass]()
+        //var user: User!
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return GroupMessages.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            var cell: MessageTableViewCell
+            if (GroupMessages[indexPath.row]._userId == CurrentPlayer.user_id) {
+                cell = tableView.dequeueReusableCell(withIdentifier: "outgoingCell") as! MessageTableViewCell
+                // cell.usernameLabel.text = CurrentPlayer.from
+                GroupMessages[indexPath.row]._from = CurrentPlayer.from
+            } else {
+                cell = tableView.dequeueReusableCell(withIdentifier: "incomingCell") as! MessageTableViewCell
+                //cell.usernameLabel.text = SelectedPlayer.from
+                //GroupMessages[indexPath.row]._from = SelectedPlayer.from
+            }
+            print(GroupMessages[indexPath.row]._from)
+            cell.configureCell(message: GroupMessages[indexPath.row])
+            return cell
+        }
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            //SocketManger.shared.
+            SocketManger.shared.connect()
+            tableView.delegate = self
+            tableView.dataSource = self
+            http.delegate = self
+            registerUser()
+            messageTextField.delegate = self
+            
+            NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardOpen), name: UIResponder.keyboardWillShowNotification, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardOpen), name: UIResponder.keyboardWillHideNotification, object: nil)
+            
+            let tapGuesterHideKeyboard = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+            let tapGuesterShowKeyboard = UITapGestureRecognizer(target: self, action: #selector(showKeyboard))
+            tableView.addGestureRecognizer(tapGuesterHideKeyboard)
+            messageTextField.superview?.addGestureRecognizer(tapGuesterShowKeyboard)
+            
+            
+            // Custom active user label
+            activeUserLabel = UILabel()
+            activeUserLabel.textColor = UIColor(red: 24/255, green: 186/255, blue: 0/255, alpha: 1.0)
+            activeUserLabel.text = "0 user"
+            activeUserLabel.sizeToFit()
+            navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: activeUserLabel)]
+            navigationItem.leftBarButtonItems = [UIBarButtonItem(title: "Re-login", style: .plain, target: self, action: #selector(reLogin))]
+            
+            SocketManger.shared.onConnect {
+                self.registerUser()
+            }
+            
+            
+//            SocketManger.shared.handleNewGroupMessage { (message) in
+//                print(message)
+//
+//                    self.GroupMessages.append(message)
+//                    self.tableView.reloadData()
+//                    self.scrollToBottomOfChat()
+//            }
+            
+            
+            SocketManger.shared.handleNewGroupMessage { (message) in
+                print(message)
+                
+            }
+            
+            SocketManger.shared.handleActiveUserChanged { (count) in
+                self.activeUserLabel.text = "\(count) user\(count > 1 ? "s" : "")"
+                self.activeUserLabel.sizeToFit()
+            }
+            
+            SocketManger.shared.handleUserTyping {
+                self.title = "Typing..."
+            }
+            
+            SocketManger.shared.handleUserStopTyping {
+                self.title = "Socket Chat"
+            }
+        }
+        
+        
+        @objc func reLogin() {
+            //UserDefaults.standard.removeObject(forKey: "username")
+            // SocketManger.shared.disconnect()
+            //  SocketManger.shared.connect()
+            self.dismiss(animated: false, completion: nil)
+        }
+        
+        func scrollToBottomOfChat(){
+            let indexPath = IndexPath(row: GroupMessages.count - 1, section: 0)
+            tableView.scrollToRow(at: indexPath, at: .bottom, animated: false)
+        }
+        
+        @IBAction func DismissView(_ sender: Any) {
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        func loadChatHistory() {
+        //http://172.107.175.8/api/get-group-message?group_id=74
+            let AccessToken = UserDefaults.standard.string(forKey: "access_token")!
+            let token_type = UserDefaults.standard.string(forKey: "token_type")!
+            print("\(token_type) \(AccessToken)")
+            let headers = [
+                
+                "Authorization" : "\(token_type) \(AccessToken)",
+            ]
+            print(GroupID)
+            http.Get(url: "\(APIConstants.GetGroupMessage)?group_id=\(GroupID)", parameters:[:], Tag: 1, headers: headers)
+            
+        }
+        
+        func registerUser() {
+            let userId = AppCommon.sharedInstance.getJSON("Profiledata")["id"].stringValue
+            if let username = AppCommon.sharedInstance.getJSON("Profiledata")["name"].string {
+                if self.CurrentPlayer == nil {
+                    self.CurrentPlayer = User(
+                        user_id: userId,
+                        from: username
+                    )
+                    //   SocketManger.shared.userJoinOnConnect(user: user)
+                    self.loadChatHistory()
+                }
+            } else {
+                
+                if let text = AppCommon.sharedInstance.getJSON("Profiledata")["name"].string  {
+                    if text != "" {
+                        self.CurrentPlayer = User(
+                            user_id: userId,
+                            from: text
+                        )
+                        UserDefaults.standard.set(text, forKey: "username")
+                        // SocketManger.shared.userJoinOnConnect(user: self.user)
+                        self.loadChatHistory()
+                    }
+                }
+            }
+            print("ddd")
+        }
+        
+        
+        // Send Message
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            sendMessage()
+            return true
+        }
+        
+        @IBAction func sendButtonDidTap(_ sender: UIButton) {
+            sendMessage()
+        }
+        
+        func sendMessage() {
+            if let message = messageTextField.text {
+                if message.trimmingCharacters(in: .whitespaces) != "" {
+                    let msg = GroupMessageModelClass(
+                        id: "",
+                        userId: CurrentPlayer.user_id,
+                        from: CurrentPlayer.from,
+                        message: message.trimmingCharacters(in: .whitespaces),
+                        created_at: ""
+                    )
+                    SocketManger.shared.sendMessage(message: msg , GroupID: GroupID )
+                    messageTextField.text = ""
+                    print(msg)
+                    GroupMessages.append(msg)
+                    tableView.reloadData()
+                    scrollToBottomOfChat()
+                }
+            }
+        }
+        // End Send Message
+        
+        
+        // Keyboard handler
+        @objc func hideKeyboard() {
+            messageTextField.resignFirstResponder()
+        }
+        
+        @objc func showKeyboard() {
+            messageTextField.becomeFirstResponder()
+        }
+        
+        @objc func handleKeyboardOpen(notification: Notification) {
+            if let userInfo = notification.userInfo {
+                if messageTextField.isEditing {
+                    let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+                    if notification.name == UIResponder.keyboardWillShowNotification {
+                        inputContainerBottomContraint.constant = -keyboardFrame.height
+                    } else {
+                        inputContainerBottomContraint.constant = 0
+                    }
+                    
+                    UIView.animate(withDuration: 0, delay: 0, options: .curveEaseOut, animations: {
+                        self.view.layoutIfNeeded()
+                    }, completion: nil)
+                }
+            }
+        }
+        
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            messageTextField.resignFirstResponder()
+        }
+        // End Keyboard handler
+        
     }
+    extension ChatRoomVC: HttpHelperDelegate {
+        func receivedResponse(dictResponse: Any, Tag: Int) {
+            print(dictResponse)
+            AppCommon.sharedInstance.dismissLoader(self.view)
+            let json = JSON(dictResponse)
+            print(json)
+            if Tag == 1 {
+                
+                let status =  json["status"]
+                 let data = json["data"]
+                print(data)
+                if status.stringValue == "1" {
+                    let result =  data.arrayValue
+                    
+                    //    DispatchQueue.main.async {
+                    
+                    self.GroupMessages.removeAll()
+                    self.tableView.reloadData()
+                    //  }
+                    
+                    for json in result {
+                        print(json)
+                        let message = GroupMessageModelClass(
+                            id: json["id"].stringValue,
+                            userId: json["user_id"].stringValue,
+                            from: json["from"].stringValue,
+                            message: json["message"].stringValue,
+                            created_at: json["created_at"].stringValue
+                            
+                        )
+                        print(message)
+                        
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+                    self.GroupMessages.append(message)
+//                        }
+                        
+                    }
+                    
+                    
+                    
+//                    var reversedMessage = [MessageModelClass]()
+//
+//                    for arrayIndex in stride(from: messages.count - 1, through: 0, by: -1) {
+//                        reversedMessage.append(messages[arrayIndex])
+//                    }
+//
+//                    messages = reversedMessage
+                    
+                    // DispatchQueue.main.async {
+                    
+                    self.tableView.reloadData();                                                     self.scrollToBottomOfChat()
+                    // }
+                    
+                    
+                }
+            }
+        }
+        
+        func receivedErrorWithStatusCode(statusCode: Int) {
+            print(statusCode)
+            AppCommon.sharedInstance.alert(title: "Error", message: "\(statusCode)", controller: self, actionTitle: AppCommon.sharedInstance.localization("ok"), actionStyle: .default)
+            
+            AppCommon.sharedInstance.dismissLoader(self.view)
+        }
+        func retryResponse(numberOfrequest: Int) {
+            
+        }
 }
 
-extension ChatRoomVC :UITableViewDelegate,UITableViewDataSource{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-        
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "SendCell", for: indexPath) as! SendCell
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        
-        print(123)
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 82
-    }
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-}
 
